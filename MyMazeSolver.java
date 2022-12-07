45cbc99c627ca3069072c6b62633a5d757512ec5
Alex Ceithamer <alexceithamer@gmail.com>
2022-12-07 16:31:12 -0600

    Added Override method headers and some private variables just to get started

--- MyMazeSolver.java
+++ MyMazeSolver.java
@@ -5,7 +5,24 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class MyMazeSolver extends MazeSolver
-{
+public class MyMazeSolver extends MazeSolver {
+    private static final int NORTH = 0;
+    private static final int EAST = 1;
+    private static final int SOUTH = 2;
+    private static final int WEST = 3;
+    private int currentWeight = 0;
+    private int turn = 0;
     
+    public int takeTurn() {
+        return -1;
+    }
+    
+    
+    public void reset() {
+        currentWeight = 0;
+    }
+    
+    public String getName() {
+        return "Human";
+    }
 }
